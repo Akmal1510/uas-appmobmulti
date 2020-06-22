615275694dd97712efc1f946899554d19b3041f4
Akmal1510 <baghdadakmal1510@gmail.com>
2020-06-22 08:31:08 +0700

    UAS Aappmobmulti

---
+++ src/providers/service/service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ServiceProvider {
+  API_KEY = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=dd06be2a7ef3454cb162814bed0055e7';
+
+  constructor(public httpClient: HttpClient) {
+    console.log('Hello ServiceProvider Provider');
+  }
+
+  getNews(){
+    return this.httpClient.get(this.API_KEY);
+  }
+
+}
+
+
+
